498c291eb4fa1a9278abaad51905dd7dcc2a4b21
Shlok Sharma <76779160+ShlokSharma-2662@users.noreply.github.com>
2022-04-21 15:58:01 +0530

    Add files via upload

---
+++ CarAPI/CarAPI.Common.Enum/CarColor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarAPI.Common.Enum
+{
+    public enum CarColor
+    {
+        Black = 1,
+        White = 2,
+        Red = 3,
+        Green = 4,
+        Blue = 5
+    }
+}
