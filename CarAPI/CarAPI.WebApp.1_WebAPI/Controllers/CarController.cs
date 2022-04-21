ad39c82ac020b2a76f2e0ef3b62e7bd85d709aa6
Shlok Sharma <76779160+ShlokSharma-2662@users.noreply.github.com>
2022-04-21 15:59:29 +0530

    Add files via upload

---
+++ CarAPI/CarAPI.WebApp.1_WebAPI/Controllers/CarController.cs
@@ -0,0 +1,63 @@
+﻿using CarAPI.Common.Models;
+using CarAPI.WebApp._2_Services.ICarServicesInterface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarAPI.WebApp._1_WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/SearchCar")]
+    public class CarController : Controller
+    {
+        private ICarServices _icarservices;
+        public CarController(ICarServices _icarservices)
+        {
+            this._icarservices = _icarservices;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<Car>))]
+        [Route("api/SearchCar")]
+        public async Task<IActionResult> Get(CarSearchParameters carSearchParameters)
+        {
+            var car = await this._icarservices.CarSearchAsync(carSearchParameters);
+            if (car == null || !car.Any())
+            {
+                return NotFound();
+            }
+            return Json(car);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(200, Type = typeof(List<Car>))]
+        [Route("api/InsertCar")]
+        public async Task<IActionResult> Post([FromBody]Car car)
+        {
+            if(await _icarservices.CarSaveAsync(car))
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Unable To Insert New Car Data");
+            }
+        }
+
+
+        [HttpDelete("{id:int}")]
+        [Route("api/DeleteCar")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            if (await _icarservices.DeleteCar(id))
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Unable To Delete Car Data");
+            }
+        }
+
+
+    }
+}
