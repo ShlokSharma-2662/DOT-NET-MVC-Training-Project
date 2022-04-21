ead4c7fdf3d5456901ec5928db2fc3c5fdcf4081
Shlok Sharma <76779160+ShlokSharma-2662@users.noreply.github.com>
2022-04-21 16:01:37 +0530

    Add files via upload

---
+++ CarAPI/CarAPI.WebApp.2_Services/CarServices/CarServices.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarAPI.Common.Models;
+using CarAPI.WebApp._2_Services.ICarServicesInterface;
+using CarAPI.WebApp._2_Services.CarServices;
+using CarAPI.WebApp._3_BusisnessLogic.IBusisnessLogic;
+
+namespace CarAPI.WebApp._2_Services.CarServices
+{
+    public class CarServices : ICarServices
+    {
+        private ICarBusisnessLogic _icarBusisnessLogic;
+
+        public CarServices(ICarBusisnessLogic _icarBusisnessLogic)
+        {
+            this._icarBusisnessLogic = _icarBusisnessLogic;
+        }
+
+
+        public async Task<List<Car>> CarSearchAsync(CarSearchParameters carSearchParameters)
+        {
+            return await this._icarBusisnessLogic.SearchCarAsync(carSearchParameters);
+        }
+
+        public async Task<bool> CarSaveAsync(Car car)
+        {
+            return await this._icarBusisnessLogic.CarSaveAsync(car);
+        }
+
+      
+
+        public async Task<bool> DeleteCar(int id)
+        {
+            return await this._icarBusisnessLogic.DeleteCar(id);
+        }
+    }
+}
---
+++ CarAPI/CarAPI.WebApp.2_Services/ICarServicesInterface/ICarServices.cs
@@ -0,0 +1,20 @@
+﻿using CarAPI.Common.Models;
+using CarAPI.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarAPI.WebApp._2_Services.ICarServicesInterface
+{
+    public interface ICarServices
+    {
+        Task <List<Car>>CarSearchAsync(CarSearchParameters carSearchParameters);
+        
+
+
+        Task<bool> CarSaveAsync(Car car);
+        Task<bool> DeleteCar(int id);
+    }
+}
