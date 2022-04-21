047d02fd59d912da9055cb0d745c4bd3b06d9653
Shlok Sharma <suttamshlok@gmail.com>
2022-04-21 16:13:51 +0530

    second commit

---
+++ MvcAdoDemo/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcAdoDemo.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public string City { get; set; }
+    }
+}
