9594382c52228872aa8cb0ac526509ecdfb1ac5c
Shlok Sharma <76779160+ShlokSharma-2662@users.noreply.github.com>
2022-04-21 15:56:56 +0530

    Add files via upload

---
+++ CarAPI/CarAPI.Commom.Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarAPI.Common.Enum;
+
+namespace CarAPI.Common.Models
+    
+{
+    public class Car
+    {
+        public int? Id { get; set; }
+        public string? CarName { get; set; }
+        public string? CarModel { get; set; }
+        public string? CarManufacturer { get; set; }
+        public CarColor? CarColor { get; set; }
+        public string? CarLaunchDate { get; set; }
+
+
+    }
+}
---
+++ CarAPI/CarAPI.Commom.Models/CarSearchParameters.cs
@@ -0,0 +1,20 @@
+﻿using CarAPI.Common.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarAPI.Common.Models
+{
+    public class CarSearchParameters
+    {
+        public string? CarName { get; set; }
+        public string? CarModel { get; set; }
+        public string? CarManufacturer { get; set; }
+        public CarColor? CarColor { get; set; }
+        public string? CarLaunchDate { get; set; }
+
+
+    }
+}
